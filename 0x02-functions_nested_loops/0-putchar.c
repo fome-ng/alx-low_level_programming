57b87a3ce4d4fde3e89452bab844d6dfea27f29d
fome-ng <fomemede@gmail.com>
2022-10-18 15:41:13 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - program that prints _putchar followed by a new line.
  * code by fome
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -16,5 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
