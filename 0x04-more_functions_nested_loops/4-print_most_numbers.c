93e018f97d90eea873e42df9c2516c107850af74
fome-ng <fomemede@gmail.com>
2022-10-23 15:25:02 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#incude "main.h"
+
+/**
+ * print _numbers - print numbers from 0 - 9
+ * Return: void
+ */
+
+void print_most_numbers(void);
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar ('\n');
+}
