da2827637ce1bbe0e82a97af1e0bae94c78f6d8d
fome-ng <fomemede@gmail.com>
2022-11-03 09:03:27 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * int _strlen_recursion - a function that returns the length of a string
- * @s: string length
+ * @s: pointer to string params
  * Return: recursion
  */
 
@@ -11,6 +11,6 @@ int _strlen_recursion(char *s)
 	if (!*s)
 	{
 		return (0);
-	}	
+	i}
 	return (1 + _strlen_recursion(++s));
 }
