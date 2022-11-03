50a9c09afccf578a2286d1f6e89a2cf8b524f7bf
fome-ng <fomemede@gmail.com>
2022-11-03 07:07:48 -0700

    task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
  * _puts_recursion - prints a string
  * @s: string
  * Return: void
  */
+ !
 void _puts_recursion(char *s)
 {
-	if (*s)
-{
+	if (*s != '\0')
+	{
 	_putchar(*s);
 	_puts_recursion(s + 1);
-}
+	}	
 else
-{
+	{
 	_putchar('\n');
-}
+	}
 
 }
 void _puts_recursion(char *s);
