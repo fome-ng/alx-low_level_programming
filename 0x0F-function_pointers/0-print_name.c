2bb2f6585db4f8a83a5109e435c31b444c63ccb5
fome-ng <fomemede@gmail.com>
2022-11-18 04:04:24 -0800

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * @print_name - Write a function that prints a name
- * @f: function of name
+ * print_name - Write a function that prints a name
+ * @name: name given
+ * @f: funtion of name
  *
  * Return: 0
  */
-
 void print_name(char *name, void (*f)(char *));
 {
 	if (name != NULL && f != NULL)
